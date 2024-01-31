03ccfe78bfd2a25ad9d2f5d974caf25f9b48c145
sugheerth <ssugheerth2096@gmail.com>
2024-01-31 17:59:52 -0500

    trying with videos

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -15,7 +15,7 @@ import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
+import porfolioImage2 from '../images/portfolio/Astar_global_plan.mp4';
 import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
 import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
 import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
